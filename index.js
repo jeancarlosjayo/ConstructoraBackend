b8e0382ece906caa8a8381429bb8e8c0b5df7e3c
Jean Carlos Ramirez Jayo <49418665+jeancarlosjayo@users.noreply.github.com>
2021-01-27 02:59:38 -0500

    dgdg
    dgdgdg

--- index.js
+++ index.js
@@ -5,17 +5,7 @@ const { obtenerDataObraUnoEntrada,obtenerHoraEntrada } = require('./obras/obreUn
 const app = express()
 
 
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
-app.listen(5000, async ()=>{
+app.listen(3000, async ()=>{
 
     let hora = 0
     let minuto = 0

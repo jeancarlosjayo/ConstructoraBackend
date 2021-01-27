4f9e202a9e71065b84c717b4b96c020d82630ff7
GerardoQ <gerardoquispe65@gmail.com>
2021-01-27 15:40:05 -0500

    if prueba

--- index.js
+++ index.js
@@ -18,22 +18,49 @@ app.listen(3000, async ()=>{
     // console.log(hora)
     // console.log(minuto)
 
-    cron.schedule(`*/1 * * * *`,async() => {
-        console.log('entro al cron de 1 min')
+    // cron.schedule(`*/1 * * * * *`,async() => {
+    //     console.log('entro al cron de 1 min')
+    //     await obtenerHoraEntrada().then(res => {
+    //         console.log('buscando hora ebntrada')
+    //         const hora = (res.horaEntrada ).toString()
+    //         const minuto = (res.minutoEntrada).toString()
+    //         console.log('sad',hora)
+    //         console.log(minuto)
+    //         cron.schedule(`${minuto} ${hora} * * *`,() => {
+    //             console.log('cron para el envio de email')
+    //             obtenerDataObraUnoEntrada()
+    //             console.log('entro')
+    //         })
+    //     }).catch(ex => {
+    //         console.log(ex)
+    //     })
+    // })
+
+
+    const prueba = async() => {
         await obtenerHoraEntrada().then(res => {
             console.log('buscando hora ebntrada')
-            const hora = res.horaEntrada + 5 
-            const minuto = res.minutoEntrada
+            const hora = (res.horaEntrada ).toString()
+            const minuto = (res.minutoEntrada).toString()
             console.log('sad',hora)
             console.log(minuto)
-            cron.schedule(`${minuto} ${hora} * * *`,() => {
-                console.log('cron para el envio de email')
+            const horaActual = new Date().getHours()
+            const minutoActual = new Date().getMinutes()
+            const horarioActual = `${horaActual}:${minutoActual}`
+            const horaFirebsae = `${hora}:${minuto}`
+            console.log('hora firebase',horaFirebsae)
+            console.log('hora actual',horarioActual)
+            console.log('coinciden',horarioActual === horaFirebsae)
+            if(horarioActual === horaFirebsae){
+                console.log('coincidieron las horas')
                 obtenerDataObraUnoEntrada()
-                console.log('entro')
-            })
+            }
+        }).catch(ex => {
+            console.log('error',ex)
         })
-    })
+    }
 
+    prueba()
     
     
     // cron.schedule(`${minuto} ${hora} * * *`,() => {

65eae341a0a6050913a3ed67ba7fbd728ad49b84
GerardoQ <gerardoquispe65@gmail.com>
2021-01-27 08:34:07 -0500

    con fe xd

--- index.js
+++ index.js
@@ -8,38 +8,48 @@ const app = express()
 
 
 app.listen(3000, async ()=>{
+    console.log('hola')
+    // await obtenerHoraEntrada().then(res => {
+    //     hora = res.horaEntrada 
+    //     minuto = res.minutoEntrada
+    //     console.log(hora)
+    //     console.log(minuto)
+    // })
+    // console.log(hora)
+    // console.log(minuto)
 
-    let hora = 0
-    let minuto = 0
-
-    await obtenerHoraEntrada().then(res => {
-        hora = res.horaEntrada + 5
-        minuto = res.minutoEntrada
-        console.log(hora)
-        console.log(minuto)
+    cron.schedule(`*/1 * * * *`,async() => {
+        await obtenerHoraEntrada().then(res => {
+            const hora = res.horaEntrada 
+            const minuto = res.minutoEntrada
+            console.log('sad',hora)
+            console.log(minuto)
+            cron.schedule(`${minuto} ${hora} * * *`,() => {
+                obtenerDataObraUnoEntrada()
+                console.log('entro')
+            })
+        })
     })
-    console.log(hora)
-    console.log(minuto)
     
-    cron.schedule(`${minuto} ${hora} * * *`,() => {
-        obtenerDataObraUnoEntrada()
-    })
+    // cron.schedule(`${minuto} ${hora} * * *`,() => {
+    //     obtenerDataObraUnoEntrada()
+    // })
 
-    cron.schedule(`${minuto} 5 * * *`,() => {
-        obtenerDataObraUnoEntrada()
-    })
+    // cron.schedule(`${minuto} 5 * * *`,() => {
+    //     obtenerDataObraUnoEntrada()
+    // })
 
-    cron.schedule(`${minuto} 7 * * *`,() => {
-        obtenerDataObraUnoEntrada()
-    })
+    // cron.schedule(`${minuto} 7 * * *`,() => {
+    //     obtenerDataObraUnoEntrada()
+    // })
 
-    cron.schedule(`30 6 * * *`,() => {
-        obtenerDataObraUnoEntrada()
-    })
+    // cron.schedule(`30 6 * * *`,() => {
+    //     obtenerDataObraUnoEntrada()
+    // })
 
-    cron.schedule(`38 6 * * *`,() => {
-        obtenerDataObraUnoEntrada()
-    })
+    // cron.schedule(`38 6 * * *`,() => {
+    //     obtenerDataObraUnoEntrada()
+    // })
 
  
     // cron.schedule(' 0 0/10 * * *',() => {

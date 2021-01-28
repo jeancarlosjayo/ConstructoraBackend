2ea74a4a21a326881398481e4cf1b0231b44243d
GerardoQ <gerardoquispe65@gmail.com>
2021-01-27 23:53:16 -0500

    entrada y salida

--- index.js
+++ index.js
@@ -1,93 +1,70 @@
 const  express = require('express')
-const nodemailer = require('nodemailer')
-const cron = require('node-cron');
-const { obtenerDataObraUnoEntrada,obtenerHoraEntrada } = require('./obras/obreUno');
+const { obtenerDataObraUnoEntrada,obtenerHoraEntrada, obtenerHoraSalida, obtenerDataObraUnoSalida } = require('./obras/obreUno');
 const app = express()
 
 
 
 
 app.listen(3000, async ()=>{
-    console.log('hola')
-    // await obtenerHoraEntrada().then(res => {
-    //     hora = res.horaEntrada 
-    //     minuto = res.minutoEntrada
-    //     console.log(hora)
-    //     console.log(minuto)
-    // })
-    // console.log(hora)
-    // console.log(minuto)
-
-    // cron.schedule(`*/1 * * * * *`,async() => {
-    //     console.log('entro al cron de 1 min')
-    //     await obtenerHoraEntrada().then(res => {
-    //         console.log('buscando hora ebntrada')
-    //         const hora = (res.horaEntrada ).toString()
-    //         const minuto = (res.minutoEntrada).toString()
-    //         console.log('sad',hora)
-    //         console.log(minuto)
-    //         cron.schedule(`${minuto} ${hora} * * *`,() => {
-    //             console.log('cron para el envio de email')
-    //             obtenerDataObraUnoEntrada()
-    //             console.log('entro')
-    //         })
-    //     }).catch(ex => {
-    //         console.log(ex)
-    //     })
-    // })
-
-
-    const prueba = async() => {
+ 
+    const ObraUnoEntrada = async() => {
         await obtenerHoraEntrada().then(res => {
-            console.log('buscando hora ebntrada')
+
             const hora = (res.horaEntrada ).toString()
+
             const minuto = (res.minutoEntrada).toString()
-            console.log('sad',hora)
-            console.log(minuto)
+
             const horaActual = new Date().getHours()
+
             const minutoActual = new Date().getMinutes()
+
             const horarioActual = `${horaActual}:${minutoActual}`
+
             const horaFirebsae = `${hora}:${minuto}`
-            console.log('hora firebase',horaFirebsae)
-            console.log('hora actual',horarioActual)
-            console.log('coinciden',horarioActual === horaFirebsae)
+
             if(horarioActual === horaFirebsae){
-                console.log('coincidieron las horas')
+                console.log('envio email hora entrada')
                 obtenerDataObraUnoEntrada()
             }
+
+        }).catch(ex => {
+            console.log('error',ex)
+        })
+
+
+    }
+
+    const ObraUnoSalida = async() => {
+        await obtenerHoraSalida().then(res => {
+
+            const hora = (res.horaSalida ).toString()
+
+            const minuto = (res.minutoSalida).toString()
+
+            const horaActual = new Date().getHours()
+
+            const minutoActual = new Date().getMinutes()
+
+            const horarioActual = `${horaActual}:${minutoActual}`
+
+            const horaFirebsae = `${hora}:${minuto}`
+
+            if(horarioActual === horaFirebsae){
+                console.log('envio email hora salida')
+                obtenerDataObraUnoSalida()
+            }
+
         }).catch(ex => {
             console.log('error',ex)
         })
     }
 
-    prueba()
+
+
+    ObraUnoEntrada()
+    ObraUnoSalida()
     
     
-    // cron.schedule(`${minuto} ${hora} * * *`,() => {
-    //     obtenerDataObraUnoEntrada()
-    // })
-
-    // cron.schedule(`${minuto} 5 * * *`,() => {
-    //     obtenerDataObraUnoEntrada()
-    // })
-
-    // cron.schedule(`${minuto} 7 * * *`,() => {
-    //     obtenerDataObraUnoEntrada()
-    // })
-
-    // cron.schedule(`30 6 * * *`,() => {
-    //     obtenerDataObraUnoEntrada()
-    // })
-
-    // cron.schedule(`38 6 * * *`,() => {
-    //     obtenerDataObraUnoEntrada()
-    // })
-
- 
-    // cron.schedule(' 0 0/10 * * *',() => {
-    //     envioEmail()
-    // })
-
     
     
 })
--- obras/obreUno.js
+++ obras/obreUno.js
@@ -2,7 +2,7 @@ const {db} = require('../firebase')
 const nodemailer = require('nodemailer')
 const express = require('express')
 const hbs = require('nodemailer-express-handlebars')
-
+const { fechaDehoy , fechaTimeStamp} = require('../helpers/Fechas')
 const ID_OBRA='1610679134'
 
 const app = express()
@@ -26,17 +26,23 @@ const obtenerDataObraUnoEntrada = async () => {
     })
 
     await db.ref('/obras/'+ ID_OBRA).child('asistencia').orderByKey().limitToLast(1).once('value',function(snapshot){
-        if(snapshot.exists()){
-            console.log('entro')
-            snapshot.forEach(
-                function(ChildSnapshot){
-                    const datafor = ChildSnapshot.val()
-                    console.log(datafor)
-                    data = Object.values(datafor)
-                    envioEmailObraUno(data,email,nameObra)
-                    // envioEmail()
-              }
-            ) 
+        if(snapshot.exists()){       
+                snapshot.forEach(
+                    function(ChildSnapshot){
+                        const timestamp  =  ChildSnapshot.key 
+                        const fecha = new Date(timestamp*1000)
+                        const fechaFirebase = fechaTimeStamp(fecha)
+                        const fechaHoy = fechaDehoy()                       
+                        const datafor = ChildSnapshot.val()
+                        data = Object.values(datafor)
+                        if( fechaHoy === fechaFirebase){
+                            console.log('envio de email')
+                            envioEmailObraUnoEntrada(data,email,nameObra)
+                        }
+                  }
+                ) 
+          
+           
         }else{
             console.log('error')
 
@@ -46,7 +52,7 @@ const obtenerDataObraUnoEntrada = async () => {
     })
 }
 
-const envioEmailObraUno = async(data,email,nameObra) => {
+const envioEmailObraUnoEntrada = async(data,email,nameObra) => {
     let mes = new Date().getMonth() + 1
     if(mes < 10){
         mes = `0${mes}`
@@ -101,7 +107,8 @@ const envioEmailObraUno = async(data,email,nameObra) => {
         context:{
             data: data,
             nameObra:nameObra,
-            diaString:diaString
+            diaString:diaString,
+            tipo:'entrada'
         }
     } 
     await transporter.sendMail(
@@ -116,6 +123,122 @@ const envioEmailObraUno = async(data,email,nameObra) => {
         }
     )
 }
+
+
+const obtenerDataObraUnoSalida = async () => {
+    console.log('entro salida ')
+    let data = []
+    let email = ''
+    let nameObra = ''
+      await db.ref('/obras/'+ ID_OBRA).once('value',function(snapshot){
+          if(snapshot.exists()){
+              console.log('entro')
+              email = snapshot.val().reportemail
+              nameObra = snapshot.val().name
+              console.log(email)
+          }else{
+              console.log('error')
+  
+              return null
+          }
+      })
+  
+      await db.ref('/obras/'+ ID_OBRA).child('asistencia').orderByKey().limitToLast(1).once('value',function(snapshot){
+          if(snapshot.exists()){       
+                  snapshot.forEach(
+                      function(ChildSnapshot){
+                          const timestamp  =  ChildSnapshot.key 
+                          const fecha = new Date(timestamp*1000)
+                          const fechaFirebase = fechaTimeStamp(fecha)
+                          const fechaHoy = fechaDehoy()                       
+                          const datafor = ChildSnapshot.val()
+                          data = Object.values(datafor)
+                          if( fechaHoy === fechaFirebase){
+                              console.log('envio de email')
+                              envioEmailObraUnoSalida(data,email,nameObra)
+                          }
+                    }
+                  ) 
+          }else{
+              console.log('error')
+  
+              return null
+          }
+      })
+  }
+  
+  const envioEmailObraUnoSalida = async(data,email,nameObra) => {
+      let mes = new Date().getMonth() + 1
+      if(mes < 10){
+          mes = `0${mes}`
+      }
+      let dia = new Date().getDate()
+      if(dia < 10){
+          dia = `0${dia}`
+      }
+      let year = new Date().getFullYear()
+      let diaString = `${dia}/${mes}/${year}`
+  
+      const dateTime = Date.now();
+      const timestamp = Math.floor(dateTime / 1000)
+  
+      app.use(express.static(__dirname + "/views"));
+      let transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          port: 465,
+          secure: true,
+          auth: {
+              user:'gerardoquispe65@gmail.com',
+              pass:'dgubhaaendarolvo'
+          },
+        });
+  
+      transporter.verify().then(res => {
+          console.log('ready')
+      }).catch(ex => {
+          console.log('ex',ex)
+      })
+  
+      transporter.use(
+          "compile",
+          hbs({
+            viewEngine: {
+              extname: ".hbs",
+              layoutsDir: "views/emails",
+              defaultLayout: "PruebaSalida",
+              partialsDir: "views/emails",
+            },
+            viewPath: "views/emails",
+            extName: ".hbs",
+          })
+        );
+  
+  
+      let emailOptionSalida ={
+          from:'Reporte de salida gerardoquispe65@gmail.com',
+          to: `gerardoquispe65@gmail.com,${email}`,
+          subject:`${timestamp} - REPORTE DE ASISTENCIA DE SALIDA  ${nameObra} `,
+          template:'PruebaSalida',
+          context:{
+              data: data,
+              nameObra:nameObra,
+              diaString:diaString,
+          }
+      } 
+      await transporter.sendMail(
+        emailOptionSalida,
+          function(err,info) {
+              if(err){
+                  console.log('errorada',err)
+                  return false
+              }else{
+                  console.log(info.response)
+              }
+          }
+      )
+  }
+
+
 
 const obtenerHoraEntrada = async () => {
     let horaEntrada = ''
@@ -138,7 +261,30 @@ const obtenerHoraEntrada = async () => {
     }
 }
 
+const obtenerHoraSalida = async () => {
+    let horaSalida = ''
+    let minutoSalida = ''
+    await db.ref('/obras/'+ ID_OBRA).once('value',function(snapshot){
+        if(snapshot.exists()){
+            const horaBD = snapshot.val().timefinish
+            const ArrayHora = horaBD.split(':')
+            horaSalida = parseInt(ArrayHora[0])
+            minutoSalida = parseInt(ArrayHora[1])
+        }else{
+            console.log('error')
+
+            return null
+        }
+    })
+    return {
+        horaSalida,
+        minutoSalida
+    }
+}
+
 module.exports={
     obtenerDataObraUnoEntrada,
-    obtenerHoraEntrada
+    obtenerHoraEntrada,
+    obtenerDataObraUnoSalida,
+    obtenerHoraSalida
 }

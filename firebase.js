a7cb51d45c55b66b5d529e70022689510d58b47c
Jean Carlos Ramirez Jayo <49418665+jeancarlosjayo@users.noreply.github.com>
2021-01-28 22:15:42 -0500

    gag supremo owo

--- firebase.js
+++ firebase.js
@@ -1,10 +1,9 @@
 
-const jsonfibase = require('./firebasedata/constructorapp-fdffb-firebase-adminsdk-h2cod-ecb73572bf.json')
-
 const admin = require('firebase-admin')
+const jsonfibase = require("./constructorapp-fdffb-firebase-adminsdk-h2cod-ecb73572bf.json")
 
 admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
+  credential: admin.credential.cert(jsonfibase),
   databaseURL:'https://constructorapp-fdffb-default-rtdb.firebaseio.com'
 })
 

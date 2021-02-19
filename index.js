5ce0145fb158ceaf78d4b4a0ec71bf8569ff0990
GerardoQ <gerardoquispe65@gmail.com>
2021-02-19 00:07:11 -0500

    clve mas salida

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const  express = require('express')
+const express = require('express')
 const app = express()
 var cron = require('node-cron');
-const { ReporteEntraObras } = require('./obras/ReporteEntrada');
-const { ReporteSalidaObras } = require('./obras/ReporteSalida');
- 
+const {
+    ReporteEntraObras
+} = require('./obras/ReporteEntrada');
+const {
+    ReporteSalidaObras
+} = require('./obras/ReporteSalida');
 
-app.listen(4000, async ()=>{
+
+app.listen(4000, async () => {
 
 
     cron.schedule('*/1 * * * *', () => {
         console.log('entro al node cron xd')
         ReporteEntraObras()
-        // ReporteSalidaObras()
+        ReporteSalidaObras()
     });
-      
-  
+
+
 
 })
